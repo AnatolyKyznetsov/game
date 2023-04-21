2b95a22d7123f2f326e546a21211e475928e8f22
AnatolyKyznetsov <120422@bk.ru>
2023-04-21 22:44:10 +0700

    Инициализация проекта - установли библиотеки, настроил форматирование файлов, добавил шрифты и стили, настроил прекоммит

--- .eslintrc.js
+++ .eslintrc.js
@@ -16,5 +16,42 @@ module.exports = {
   plugins: ['@typescript-eslint'],
   rules: {
     '@typescript-eslint/ban-ts-comment': 1,
+    'indent': [
+        'error',
+        4
+    ],
+    'linebreak-style': [
+        'error',
+        'unix'
+    ],
+    'quotes': [
+        'error',
+        'single'
+    ],
+    'no-multiple-empty-lines': [
+        'error',
+        {
+            'max': 1,
+            'maxEOF': 0,
+            'maxBOF': 0
+        }
+    ],
+    'no-multi-spaces': 'error',
+    'object-curly-spacing': [
+        'error',
+        'always'
+    ],
+    'array-bracket-spacing': [
+        'error',
+        'always'
+    ],
+    'max-len': [
+        'error',
+        {
+            'code': 200,
+        }
+    ],
+    '@typescript-eslint/no-this-alias': 'off',
+    '@typescript-eslint/no-empty-function': 'off',
   },
 }
