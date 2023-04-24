73cd308b57dc5369cf258bde779357a772c4c128
Ekaterina <stramnova-kate-11@yandex.ru>
2023-04-24 18:08:05 +0400

    Login page.

---
+++ packages/client/src/pages/Login.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { Input } from '../components/Input'
+import { INPUT_TOOLTIPS } from '../components/Input'
+import { Button } from '../components/Button'
+
+export function Login() {
+    return (
+        <div className='shape'>
+            <div className='shape__wrapper'>
+                <div className='title title_main shape__title shape__title_big'>
+                    Вход
+                </div>
+                <form method='post' className='form'>
+                    <Input
+                        type='text'
+                        name='login'
+                        label='Логин'
+                        tooltip={INPUT_TOOLTIPS.login}
+                    />
+                    <Input
+                        type='password'
+                        name='password'
+                        label='Пароль'
+                        tooltip={INPUT_TOOLTIPS.password}
+                    />
+                    <Button type='submit' text='Войти' buttonClass='form__button'/>
+                </form>
+                <a href='/register' className='link shape__link'>Еще не зарегестрированы?</a>
+            </div>
+        </div>
+    )
+}
