1eab8a793bc86eda0f7f4cbd6e69b7e2bb36aa74
Ekaterina <stramnova-kate-11@yandex.ru>
2023-04-26 23:23:21 +0400

    Fixes

--- packages/client/src/utils/Validator.ts
+++ packages/client/src/utils/Validator.ts
@@ -1,35 +1,45 @@
-export function Validator(string: string, type: string): boolean {
+export function Validator(string: string | undefined, type: string): boolean {
     let reg;
     let result;
     switch (type){
     case 'first_name':
     case 'second_name':
         reg = '^[A-ZА-Я][a-zA-ZА-Яа-я\\-]*$';
-        result = string.match(reg);
+        if (string) {
+            result = string.match(reg);
+        }
         if (!result || result.length !== 1) return false
         break
     case 'login':
         reg = '^(?=.*[A-Za-z])(?:[a-zA-Z0-9_-]){3,20}$';
-        result = string.match(reg);
+        if (string) {
+            result = string.match(reg);
+        }
         if (!result || result.length !== 1) return false
         break
     case 'email':
         reg = '^[a-z][a-z0-9-]*@[a-z]*.[a-z]*$';
-        result = string.match(reg);
+        if (string) {
+            result = string.match(reg);
+        }
         if (!result || result.length !== 1) return false
         break
     case 'phone':
         reg = '^\\+?[0-9]{10,15}$';
-        result = string.match(reg);
+        if (string) {
+            result = string.match(reg);
+        }
         if (!result || result.length !== 1) return false
         break
     case 'password':
         reg = '^(?=.*[0-9])(?=.*[a-zA-Z])(?:[a-zA-Z0-9_-]+){8,40}$';
-        result = string.match(reg);
+        if (string) {
+            result = string.match(reg);
+        }
         if (!result || result.length !== 1) return false
         break
     case 'message':
-        if (string.length < 1) return false
+        if (string && string.length < 1) return false
         break
     }
     return true
