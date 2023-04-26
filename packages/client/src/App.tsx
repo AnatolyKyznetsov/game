dce6838731935d3e0bfb075cdb5eb6e363c0024f
Ilya <ilya.yudin@metamins.com>
2023-04-26 15:47:38 +0500

    Страница с ошибками переименована в ErrorPage

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -3,7 +3,7 @@ import { LoginPage } from './pages/LoginPage'
 import { Route, Routes, useLocation } from 'react-router-dom'
 import { Paths } from './utils/paths'
 import { MainPage } from './pages/MainPage'
-import { Error } from './pages/Error'
+import { ErrorPage } from './pages/ErrorPage'
 
 export const App = () => {
     const location = useLocation()
@@ -12,8 +12,8 @@ export const App = () => {
             <Route path={Paths.register} element={<RegisterPage />} />
             <Route path={Paths.login} element={<LoginPage />} />
             <Route path={Paths.main} element={<MainPage />} />
-            <Route path={Paths.notFound} element={<Error title="404" />} />
-            <Route path={Paths.error} element={<Error title="500" />} />
+            <Route path={Paths.notFound} element={<ErrorPage title="404" />} />
+            <Route path={Paths.error} element={<ErrorPage title="500" />} />
         </Routes>
     )
 }
