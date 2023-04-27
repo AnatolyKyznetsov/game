28904dc688a74b2dba8cdd5230eb1d3f6d1e39de
Sergey Kalachev <rollstream@yandex.ru>
2023-04-28 05:52:17 +0700

    end screen added

---
+++ packages/client/src/pages/EndScreenPage.tsx
@@ -0,0 +1,43 @@
+import React, { useEffect, useRef, useState } from 'react'
+import { gsap } from 'gsap'
+import { useNavigate } from 'react-router-dom'
+import { Paths } from '../utils/paths'
+import { Button } from '../components/Button'
+
+export const EndScreenPage= () => {
+    const title = useRef(null)
+    const [ isActiveButton, setIsActiveButton ] = useState(false)
+    const navigate = useNavigate()
+
+    const handleEndButton = () => {
+        navigate(Paths.main)
+    }
+    useEffect(() => {
+        const context = gsap.context(() => {
+            gsap.to(title.current, {
+                startAt: {
+                    y: '80vh'
+                },
+                duration: 7,
+                ease: 'elastic',
+                y: '-=90vh',
+                onRepeat: () => setIsActiveButton(true),
+                repeat: 0,
+                overwrite: true,
+            })
+        })
+
+        return () => context.revert()
+    }, [])
+
+    return (
+        <main className='main' >
+            <h1 ref={title}>The End</h1>
+            {isActiveButton && <Button
+                onClick={handleEndButton}
+                type='button'
+                text='Начать с начала'
+                buttonClass='button__start' />}
+        </main>
+    )
+}
