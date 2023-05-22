8eac4a91883fb6b71779ee430a94ae034d58f627
Ilya <ilya.yudin@metamins.com>
2023-05-22 16:25:49 +0500

    Доработаны страницы профиля, 500, 404

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -28,8 +28,8 @@ export const App = () => {
             <Route path={Paths.profile} element={<ProfilePage />} />
             <Route path={Paths.editProfile} element={<EditProfilePage />} />
             <Route path={Paths.editPassword} element={<EditPasswordPage />} />
-            <Route path={Paths.notFound} element={<ErrorPage title="404" />} />
-            <Route path={Paths.error} element={<ErrorPage title="500" />} />
+            <Route path={Paths.notFound} element={<ErrorPage title="404" text="Не туда попали." />} />
+            <Route path={Paths.error} element={<ErrorPage title="500" text="Ошибка сервера, мы уже фиксим." />} />
             <Route path={Paths.leaderBoard} element={<LeaderBoardPage />} />
         </Routes>
     )
