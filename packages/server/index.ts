7b31f17d0370f33fe4ca1ae1b3a6c8c743319cac
AnatolyKyznetsov <120422@bk.ru>
2023-06-02 12:30:12 +0700

    gam-39 - Настроить Express для Server Side Rendering

--- packages/server/index.ts
+++ packages/server/index.ts
@@ -1,20 +1,77 @@
 import dotenv from 'dotenv'
 import cors from 'cors'
+import type { ViteDevServer } from 'vite';
+import { createServer as createViteServer } from 'vite'
+
 dotenv.config()
 
 import express from 'express'
-import { createClientAndConnect } from './db'
+import * as fs from 'fs'
+import * as path from 'path'
 
-const app = express()
-app.use(cors())
-const port = Number(process.env.SERVER_PORT) || 3001
+const isDev = () => process.env.NODE_ENV === 'development'
 
-createClientAndConnect()
+async function startSerever() {
+    let vite: ViteDevServer | undefined;
 
-app.get('/', (_, res) => {
-    res.json('👋 Howdy from the server :)')
-})
+    const app = express()
 
-app.listen(port, () => {
-    console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
-})
+    app.use(cors())
+
+    const port = Number(process.env.SERVER_PORT) || 3000
+    const distPath = 'packages/client/dist'
+    const srcPath = '../client/'
+
+    if (isDev()) {
+        vite = await createViteServer({
+            server: { middlewareMode: true },
+            root: srcPath,
+            appType: 'custom'
+        })
+
+        app.use(vite.middlewares)
+    } else {
+        app.use('/assets', express.static(path.resolve(distPath, 'assets')))
+        app.use('/images', express.static(path.resolve(distPath, 'images')))
+        app.use('/fonts', express.static(path.resolve(distPath, 'fonts')))
+    }
+
+    app.use('*', async (req, res, next) => {
+        const url = req.originalUrl;
+
+        try {
+            let template: string;
+
+            if (isDev()) {
+                template = fs.readFileSync(path.resolve(srcPath, 'index.html'), 'utf-8')
+                template = await vite!.transformIndexHtml(url, template)
+            } else {
+                template = fs.readFileSync(path.resolve(distPath, 'index.html'), 'utf-8')
+            }
+
+            let render: (url: string) => Promise<string>;
+
+            if (isDev()) {
+                render = (await vite!.ssrLoadModule(path.resolve(srcPath, 'ssr.tsx'))).render
+            } else {
+                render = (await import(require.resolve('../../client/dist-ssr/client.cjs'))).render
+            }
+
+            const appHtml = await render(url)
+            const html = template.replace('<!--ssr-outlet-->', appHtml)
+
+            res.status(200).set({ 'Content-Type': 'text/html' }).end(html)
+        } catch (e) {
+            if (isDev()) {
+                vite!.ssrFixStacktrace(e as Error)
+            }
+            next(e)
+        }
+    })
+
+    app.listen(port, () => {
+        console.log(`  ➜ 🎸 Server is listening on port: ${port}`)
+    })
+}
+
+startSerever()
