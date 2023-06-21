b0e25b3e9bd014317ceadad1b17bccb2e04c66a1
Ekaterina <stramnova-kate-11@yandex.ru>
2023-06-21 17:38:17 +0400

    Fixes.

--- packages/client/src/App.test.tsx
+++ packages/client/src/App.test.tsx
@@ -1,7 +1,15 @@
 import { App } from './App'
 import { render } from '@testing-library/react'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
 test('Example test', async () => {
-    render(<BrowserRouter><App /></BrowserRouter>)
+    render(
+        <BrowserRouter>
+            <Provider store={store}>
+                <App />
+            </Provider>
+        </BrowserRouter>
+    )
 })
