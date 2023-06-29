ded5a25450d5d91663d5d4d445fbc8df043dbfdb
AnatolyKyznetsov <120422@bk.ru>
2023-06-29 18:28:19 +0700

    hotfix

--- packages/server/routes/addThemeRouters.ts
+++ packages/server/routes/addThemeRouters.ts
@@ -3,21 +3,29 @@ import { ThemeService } from '../services/ThemeService';
 
 export function addThemeRouters(app: Express) {
     app.post('/local/theme_set', express.json(), async (req, res) => {
-        const { isLightTheme, userId } = req.body.data
-        const themeData = await ThemeService.find(userId)
-
-        if (themeData?.dataValues.id) {
-            ThemeService.update(isLightTheme, userId)
-        } else {
-            ThemeService.create(isLightTheme, userId)
+        try {
+            const { isLightTheme, userId } = req.body.data
+            const themeData = await ThemeService.find(userId)
+    
+            if (themeData?.dataValues.id) {
+                ThemeService.update(isLightTheme, userId)
+            } else {
+                ThemeService.create(isLightTheme, userId)
+            }
+    
+            res.send('ok')
+        } catch {
+            res.send('fail')
         }
-
-        res.send('ok')
     })
 
     app.post('/local/theme_get', express.json(), async (req, res) => {
-        const { userId } = req.body.data
-        const themeData = await ThemeService.find(userId)
-        res.send(themeData ? themeData : null)
+        try {
+            const { userId } = req.body.data
+            const themeData = await ThemeService.find(userId)
+            res.send(themeData ? themeData : null)
+        } catch {
+            res.send(null);
+        }
     })
 }
--- packages/server/routes/addUserRouters.ts
+++ packages/server/routes/addUserRouters.ts
@@ -3,14 +3,22 @@ import { UserService } from '../services/UserService';
 
 export function addUserRouters(app: Express) {
     app.post('/local/user_set', express.json(), async (req, res) => {
-        const { login } = req.body.data
-        const user = await UserService.create(login);
-        res.send({ userId: user[0].dataValues.id })
+        try {
+            const { login } = req.body.data
+            const user = await UserService.create(login);
+            res.send({ userId: user[0].dataValues.id })
+        } catch {
+            res.send({ userId: null })
+        }
     })
 
     app.post('/local/user_eidt', express.json(), async (req, res) => {
-        const { login, id } = req.body.data
-        const user = await UserService.update(login, id);
-        res.send(user)
+        try {
+            const { login, id } = req.body.data
+            const user = await UserService.update(login, id);
+            res.send(user)
+        } catch {
+            res.send(null)
+        }
     })
 }
