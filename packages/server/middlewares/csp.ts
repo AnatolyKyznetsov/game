1d6e68fbb4bd53df55de2f5313a31b44813f8d2e
Nikolay Dimitriev <dimitrievk56@gmail.com>
2023-07-14 14:59:44 +0300

    fix lint error

--- packages/server/middlewares/csp.ts
+++ packages/server/middlewares/csp.ts
@@ -7,21 +7,21 @@ export const cspMiddleware = () =>
         directives: isDev()
             ? {}
             : {
-                  'default-src': [
-                      SELF,
-                      'https://ya-praktikum.tech/api/v2',
-                      'http://localhost:3000',
-                      'https://ya-praktikum.tech/api/v2/oauth/yandex/service-id',
-                      'https://ya-praktikum.tech/api/v2/auth/user',
-                      'http://game-machine.ya-praktikum.tech',
-                  ],
-                  'font-src': [
-                      SELF,
-                      'https://fonts.googleapis.com/',
-                      'https://fonts.gstatic.com/',
-                  ],
-                  'img-src': [SELF, 'data:', 'https://ya-praktikum.tech'],
-                  'script-src': [SELF, INLINE],
-                  'style-src': [SELF, INLINE, 'https://fonts.googleapis.com/'],
-              },
+                'default-src': [
+                    SELF,
+                    'https://ya-praktikum.tech/api/v2',
+                    'http://localhost:3000',
+                    'https://ya-praktikum.tech/api/v2/oauth/yandex/service-id',
+                    'https://ya-praktikum.tech/api/v2/auth/user',
+                    'http://game-machine.ya-praktikum.tech',
+                ],
+                'font-src': [
+                    SELF,
+                    'https://fonts.googleapis.com/',
+                    'https://fonts.gstatic.com/',
+                ],
+                'img-src': [ SELF, 'data:', 'https://ya-praktikum.tech' ],
+                'script-src': [ SELF, INLINE ],
+                'style-src': [ SELF, INLINE, 'https://fonts.googleapis.com/' ],
+            },
     })
